5e8b702c798c3480aff4ce2bdccc93eb9fb04042
kramalmr <akram3107@student.abudzar.sch.id>
2024-07-30 10:19:48 +0700

    quizjs2

---
+++ QUIZJS2-akram/QUIZJS2.js
@@ -0,0 +1,33 @@
+// MUHAMMAD AKRAM ALMAIR WAKK !!
+
+// nomor 1
+var a;
+console.log(a);
+
+// nomor 2
+let name = "Muhammad Akram ALmair";
+let age = 15;
+let homeAddress = "Ciputat, Banten, Tangsel, Indonesia";
+
+let person = {
+    "Nama": name,
+    "Umur": age,
+    "Alamat": homeAddress
+};
+console.log(
+    "Nama :" + name,
+    "Umur :" + age,
+    "Alamat :" + homeAddress,
+)
+console.log(person);
+
+// nomor 3
+let noNilai = null;
+console.log(noNilai);
+
+// nomor 4
+let alas = 5;
+let tinggi = 7;
+
+let luas = 1/2 * alas * tinggi;
+console.log(luas);
