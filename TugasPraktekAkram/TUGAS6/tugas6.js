14b04254c12323dd65b4aada45cd11e030063a1a
kramalmr <akram3107@student.abudzar.sch.id>
2024-08-06 10:43:32 +0700

    tugas6
    whatchusaydaddyshome!!

---
+++ TugasPraktekAkram/TUGAS6/tugas6.js
@@ -0,0 +1,30 @@
+//TUGAS 6 MUHAMMAD AKRAM ALMAIR
+
+// SOAL 1
+let identity = ['Akram', 15, 'Tangsel'];
+console.log(identity[1]);
+
+// SOAL 2
+identity[0] = 'Almair'
+console.log(identity[0]);
+
+// SOAL 3
+identity.splice(1, 1)
+console.log(identity)
+
+// SOAL 4
+let array1 = [1, 2, 3,]
+let array2 = [4, 5, 6,]
+let array3 = array1.concat(array2)
+console.log(array3)
+
+// SOAL 5
+console.log(identity.indexOf('Tangsel'))
+
+// SOAL 6
+const numbers = [34, 7, 23, 32, 5];
+numbers.sort();
+console.log(numbers);
+
+// SOAL 7
+console.log(numbers.length)
