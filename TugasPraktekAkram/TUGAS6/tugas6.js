fdb05962f182425653359a84c26b8ba2cb1f3808
kramalmr <akram3107@student.abudzar.sch.id>
2024-08-06 10:52:15 +0700

    Update tugas6
    iknowyouvebeenwaititnf

--- TugasPraktekAkram/TUGAS6/tugas6.js
+++ TugasPraktekAkram/TUGAS6/tugas6.js
@@ -1,30 +1,35 @@
 //TUGAS 6 MUHAMMAD AKRAM ALMAIR
 
 // SOAL 1
-let identity = ['Akram', 15, 'Tangsel'];
+let identity = ["Akram", 15, "Tangsel"];
 console.log(identity[1]);
 
 // SOAL 2
-identity[0] = 'Almair'
+identity[0] = "Almair";
 console.log(identity[0]);
 
 // SOAL 3
-identity.splice(1, 1)
-console.log(identity)
+identity.splice(1, 1);
+console.log(identity);
 
 // SOAL 4
-let array1 = [1, 2, 3,]
-let array2 = [4, 5, 6,]
-let array3 = array1.concat(array2)
-console.log(array3)
+let array1 = [1, 2, 3];
+let array2 = [4, 5, 6];
+let array3 = array1.concat(array2);
+console.log(array3);
 
 // SOAL 5
-console.log(identity.indexOf('Tangsel'))
+console.log(identity.indexOf("Tangsel"));
 
 // SOAL 6
+
 const numbers = [34, 7, 23, 32, 5];
-numbers.sort();
+function compareNumbers(a, b) {
+  return a - b;
+}
+
+numbers.sort(compareNumbers);
 console.log(numbers);
 
 // SOAL 7
-console.log(numbers.length)
+console.log(numbers.length);
